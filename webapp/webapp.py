5f77322763dcf3212fb6f88023043f31e70b6d9a
Alexjmsherman <alexjmsherman@gmail.com>
2016-07-14 11:36:40 -0400

    add results.html

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -4,10 +4,6 @@ from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_wtf import Form
 from wtforms.fields import RadioField, SubmitField, SelectField, StringField
 from wtforms.validators import Required
-from whoosh_search import search
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database.database_setup import SectionTable
 from flask import Markup
 
 
@@ -16,8 +12,7 @@ app.config['SECRET_KEY'] = 'secret!'
 
 
 class Survey(Form):
-    q1 = SelectField('q1', validators=[Required()])
-    q2 = SelectField('q2', validators=[Required()])
+    q1 = StringField('q1')
     submit = SubmitField('Submit')
 
 @app.route('/index/',  methods=['GET', 'POST'])
@@ -26,13 +21,18 @@ def index():
 
     if request.method == 'POST':
         if form.validate_on_submit():
-            query = form.search.data
             q1 = form.q1.data
-            q2 = form.q2.data
 
-            return redirect(url_for('index'))
+            return render_template('results.html', q1=q1)
 
-    return render_template('index.html')
+    return render_template('index.html', form=form)
+
+
+@app.route('/index/results/',  methods=['GET', 'POST'])
+def results(q1):
+
+    return render_template('results.html', q1=q1)
+
 
 if __name__ == "__main__":
     app.debug = True
