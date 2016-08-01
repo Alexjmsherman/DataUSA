29790927e9d3b098cac2f01580e17807722808be
Alexjmsherman <alexjmsherman@gmail.com>
2016-07-31 22:38:19 -0400

    change skills from 1-5 to 0-5

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -184,7 +184,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template('index.html'), 500
 
 
 if __name__ == "__main__":
